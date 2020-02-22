36e5f8be360734277e72dc71a64a1479de42cf0f
leo <leocv0205@gmail.com>
2020-02-21 21:12:01 -0500

    punto 1 del taller 1

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 
-echo("hola mundo");
+echo "TALLER 1 GENERALIDADES PHP. " ;
+echo "<br> Elaborado por LEONARDO CARO <br>";
+
+/*Punto 1 del taller*/
+$numero1=15;
+$numero2=34;
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo "<br>Punto 1 <br>";
+echo ("<br>La suma de los numeros 15 + 34 es ".$suma);
+echo ("<br> La resta de los numeros 15 - 34 es ".$resta);
+echo ("<br> La multiplicación de los numeros 15 * 34 es ".$multiplicacion);
+echo ("<br> La división de los numeros 15 / 34 es ".$division);
+
+
 
 
 ?>
